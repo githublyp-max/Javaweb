a2f7c1e78aedf1fa10c9ebe1e12e47b694cc2a88
lyp <lyp@qq.com>
2023-02-13 20:50:56 +0800

    Default Changelist

---
+++ servlet/src/main/java/session/SessionDemo011.java
@@ -0,0 +1,45 @@
+package session;
+
+
+
+import org.apache.jasper.runtime.HttpJspBase;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.HttpJspPage;
+import java.io.IOException;
+
+
+public class SessionDemo011 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //解决乱码问题
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        
+        resp.setContentType("text/html;charset=utf-8");
+        //获取session
+        HttpSession session = req.getSession();
+        //给session存值
+        session.setAttribute("person", new Person("zyy", 18));
+        //获取session的id
+        String id = session.getId();
+        if (session.isNew()) {
+            resp.getWriter().write("session创建成功，id:" + id);
+        } else {
+            resp.getWriter().write("session已经在服务器中存在了，id:" + id);
+        }
+        resp.getWriter().flush();
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
